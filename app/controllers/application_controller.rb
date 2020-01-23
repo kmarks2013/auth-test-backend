597e5ca8606bc4fc711c5145d34c25fe6da95dbf
kmarks2013 <kmarks2013@gmail.com>
2020-01-23 15:23:44 -0500

    refactors toekn encoding to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+    def encode_token(payload)
+        JWT.encode(payload, "secretkeyexample", 'HS256')
+    end
 end
+
+# payload = {user_id: user.id}
+# token = JWT.encode(payload, "secretkeyexample", 'HS256')
+# render json: { token: token }
